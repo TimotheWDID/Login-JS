4a03872cf5ee89261a12a5d6aeba9ebbcbdc12ef
Timothe Portelli <portelli.timothe@gmail.com>
2020-05-31 21:08:25 +0200

    Update icons interface
    Add icons
    modification of the interface change system

--- src/index.js
+++ src/index.js
@@ -22,8 +22,11 @@ function importAll(r) {
 
     //>Pages
         const main = document.querySelector('main');
-        const connected = 'translateY(-100vh) translateX(-100vw)';
-        const forgetpassword = 'translateY(-100vh) translateX(100vw)';
+        const interfacelogged = document.querySelector('#connected');
+        const login = 'translateY(-100vh) translateX(00vw)';
+        const logged = 'translateY(-100vh) translateX(-100vw)';
+        const forgetpassword1 = 'translateY(-100vh) translateX(100vw)';
+        const forgetpassword2 = 'translateY(-100vh) translateX(100vw)';
         const register1 = 'translateY(00vh) translateX(00vw)';
         const register2 = 'translateY(00vh) translateX(-100vw)';
         const register3 = 'translateY(00vh) translateX(-200vw)';
@@ -47,28 +50,67 @@ function importAll(r) {
         //<Test Login
     //<Connexion page
 
-    //>Change page
+    //>Click to Change interface
 
-        document.querySelector('#login-submit').addEventListener('click', (e)=>{ //Log-in
-            e.preventDefault();
-            if (testlogin()) { //connected
-                main.style.transform = connected;
+        document.querySelectorAll('a, button').forEach(item =>{
+            item.addEventListener('click', (e) =>{
+                let goto = item.dataset.go;
                 
-            }else //wrong password or email
-                console.log('Connection failed');
-  
-        });
+                switch (goto) {
+                    case 'logged':
+                        e.preventDefault();
+                        if (testlogin())
+                            main.style.transform = logged;
+                        else
+                            console.log('Connexion failed');
+                            
+                        break;
 
-        document.querySelector('#forgetpassword').addEventListener('click', (e)=>{ //forget password
-            e.preventDefault();
-            main.style.transform = forgetpassword;
-        });
+                    case 'login':
+                        e.preventDefault();
+                        interfacelogged.style.display = flex;
+                        main.style.transform = login;
+                        break;
+                        
+                    case 'forgetpassword1':
+                        e.preventDefault();
+                        main.style.transform = forgetpassword1;
+                        break;
 
-        document.querySelector('#register').addEventListener('click', (e)=>{ //forget password
-            e.preventDefault();
-            console.log('reg');
-            
-            main.style.transform = register1;
-        });
-    //<Change page
+                    case 'forgetpassword2':
+                        e.preventDefault();
+                        main.style.transform = forgetpassword2;
+                        break;
+
+                    case 'register1':
+                        e.preventDefault();
+                        main.style.transform = register1;
+                        break;
+
+                    case 'register2':
+                        e.preventDefault();
+                        main.style.transform = register2;
+                        break;
+
+                    case 'register3':
+                        e.preventDefault();
+                        main.style.transform = register3;
+                        break;
+
+                    case 'disconnect':
+                        e.preventDefault();
+                        main.style.transform = login;
+                        interfacelogged.style.display = none;
+
+                        break;
+                    
+                    default:
+                        break;
+                }
+                
+            })
+        })
+
+    //<Click to change interface
+
 //<Functions
